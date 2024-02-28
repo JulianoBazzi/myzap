9805f36bddc8fcef607de3de8acb92c4899c424d
Juliano Bazzi <julibazzi@hotmail.com>
2024-02-27 22:07:32 -0400

    Update validations.js

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -25,7 +25,8 @@ const checkParams = async (req, res, next) => {
     else if (data.sessionkey != req.headers['sessionkey']) {
         return res.status(401).json({
             "result": 401,
-            "messages": "Não autorizado, verifique se o nome da sessão e o sessionkey estão corretos"
+            "messages": "Não autorizado, verifique se o nome da sessão e o sessionkey estão corretos",
+            "data": data
         })
     }
     else {
